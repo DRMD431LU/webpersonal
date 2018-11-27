a7619a5fd8ee39b8ef5af4529a043d43eed72227
unknown <drmdfoureilu@gmail.com>
2018-11-27 12:42:41 -0600

    adding all first part

--- core/views.py
+++ core/views.py
@@ -1,36 +1,17 @@
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
-html_base = """
-<h1> Mi web personal </h1>
-<ul>
-	<li><a href="/">Portada</a></li>
-	<li><a href="/about-me/">Acerca de</a></li>
-	<li><a href="/portfolio/">Portfolio</a></li>
-	<li><a href="/contact/">Contacto</a></li>
-</ul>
-"""
+
 def home(request):
-	return HttpResponse(html_base + """
-		<h2 Portada </h2>
-		<p>Esto es la portada</p>
-		""")
+	return render(request, "core/home.html")
 
 def about(request):
-	return HttpResponse(html_base+"""
-		<h2>Acerca de: </h2>
-		<p>perro >:v</p>
-		""")
+	return render(request, "core/about.html")
 
 def portfolio(request):
-	return HttpResponse(html_base +"""
-		<h2 Portafolio</h2>
-		<p>Trabajos :D</p>
-		""")
+	return render(request, "core/portfolio.html")
 
 def contact(request):
-	return HttpResponse(html_base+"""
-		<h2>Contact </h2>
-		<p>prueba de contacto</p>
-		""")
+	return render(request, "core/contact.html")
+
 
